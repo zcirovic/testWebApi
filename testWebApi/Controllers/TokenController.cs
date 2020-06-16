8f58307521347650eb90e8278e5ccc6ce64b7e66
zoran <zoran.cirovic@gmail.com>
2020-06-16 09:56:09 +0200

    16-6:1

---
+++ testWebApi/Controllers/TokenController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.IO;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using testWebApi.Models;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace testWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        public IConfiguration _configuration;
+        private readonly TESTContext _context;
+
+        public TokenController(IConfiguration config, TESTContext context)
+        {
+            _configuration = config;
+            _context = context;
+        }
+
+        //curl -d '{"Email":"InventoryAdmin@abc.com", "Password":"$admin@2017"}' -H "Content-Type: application/json" -X POST https://localhost:44356/api/token
+        [HttpPost]
+        public async Task<IActionResult> Post(UserInfo _userData)
+        {
+            if (_userData != null && _userData.UserName != null && _userData.Password != null)
+            {
+                var user = await GetUser(_userData.UserName, _userData.Password);
+
+                if (user != null)
+                {
+                    //create claims details based on the user information
+                    var claims = new[] {
+                    new Claim(JwtRegisteredClaimNames.Sub, _configuration["Jwt:Subject"]),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                    new Claim(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString()),
+                    new Claim("Id", user.UserId.ToString()),
+                    new Claim("FirstName", user.FirstName),
+                    new Claim("LastName", user.LastName),
+                    new Claim("UserName", user.UserName),
+                    new Claim("Email", user.Email)
+                   };
+
+                    var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+
+                    var signIn = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                    var token = new JwtSecurityToken(_configuration["Jwt:Issuer"], _configuration["Jwt:Audience"], claims, expires: DateTime.UtcNow.AddDays(1), signingCredentials: signIn);
+
+                    var user2 = new
+                    {
+                        id = 1,
+                        email="nesto",
+                        roles = new string[] { "ROLE_ADMIN", "ASDF" },
+                        username = _userData.UserName,
+                        accesstoken = new JwtSecurityTokenHandler().WriteToken(token)
+                    };
+                    var strUser2 = JsonSerializer.Serialize(user2);
+                    return Ok(strUser2);
+                    //return Ok(new JwtSecurityTokenHandler().WriteToken(token));
+                }
+                else
+                {
+                    return BadRequest("Invalid credentials");
+                }
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        private async Task<UserInfo> GetUser(string username, string password)
+        {
+            return await _context.UserInfo.FirstOrDefaultAsync(u => u.UserName == username && u.Password == password);
+        }
+    }
+}
---
+++ testWebApi/Models/TESTContext.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace testWebApi.Models
+{
+    public partial class TESTContext : DbContext
+    {
+        //public TESTContext()
+        //{
+        //}
+
+        public TESTContext(DbContextOptions<TESTContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Products> Products { get; set; }
+        public virtual DbSet<UserInfo> UserInfo { get; set; }
+
+//        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//        {
+//            if (!optionsBuilder.IsConfigured)
+//            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+//                optionsBuilder.UseSqlServer("Data Source=ZC-A7\\SQLEXPRESS;Initial Catalog=TEST;Integrated Security=True");
+//            }
+//        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Products>(entity =>
+            {
+                entity.HasKey(e => e.ProductId)
+                    .HasName("PK__Products__B40CC6CD804EEB83");
+
+                entity.Property(e => e.Category)
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Color)
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.UnitPrice).HasColumnType("decimal(18, 0)");
+            });
+
+            modelBuilder.Entity<UserInfo>(entity =>
+            {
+                entity.HasKey(e => e.UserId)
+                    .HasName("PK__UserInfo__1788CC4C947A6036");
+
+                entity.Property(e => e.CreatedDate)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.UserName)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
